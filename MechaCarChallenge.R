5b04667c3e68f102132049bd1ee39d67fd3e98bf
Zoe Ficken <88108455+zoeficken@users.noreply.github.com>
2021-11-21 19:18:47 -0500

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,5 @@
+#Deliverable 1
+
 library(dplyr)
 
 MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
@@ -6,3 +8,10 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
 
+#Deliverable 2
+
+Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- Suspension %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=(PSI),Variance=var(PSI),SD=sd(PSI))
